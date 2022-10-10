e78ec078922434830bb39b74b29adc1c56474946
Kaaarlos <ca991467@gmail.com>
2022-10-10 17:04:37 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include("template/cabecera.php"); ?>
+            <div class="jumbotron">
+                <h1 class="display-3">Bienvenedios</h1>
+                <p class="lead">Tienda de libros</p>
+                <hr class="my-2">
+                <p>More info</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="productos.php" role="button">Ver libros</a>
+                </p>
+            </div>
+<?php include("template/pie.php"); ?>
